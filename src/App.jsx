716b0f5b44d837c97c92c8618c2d5680958a9e54
VolodymyrTrush <vmtrush@gmail.com>
2022-07-08 18:50:25 +0300

    HW2

---
+++ src/App.jsx
@@ -0,0 +1,79 @@
+import { Component } from 'react';
+import Container from './components/Container';
+import ContactForm from './components/ContactForm';
+import Filter from './components/Filter';
+import ContactList from './components/ContactList';
+
+import styles from './App.module.scss';
+
+class App extends Component {
+  state = {
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  // Добавляет контакт (желательно сократить или вынести)
+  addContact = newContact => {
+    // Проверка на дубликат
+    const duplicateName = this.state.contacts.find(
+      contact => contact.name === newContact.name,
+    );
+
+    if (duplicateName) {
+      alert(`${newContact.name} is already on contacts`);
+      return;
+    }
+
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  // Следит за полем фильтрации и пишет в стейт
+  changeFilter = event => {
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  // Фильтрует и возвращает результат фильтра
+  filterContacts = () => {
+    const { contacts, filter } = this.state;
+
+    const normalizedFilter = filter.toLowerCase();
+
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter),
+    );
+  };
+
+  // Удаляет контакт
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  render() {
+    const { filter } = this.state;
+    const filteredResults = this.filterContacts();
+
+    return (
+      <Container>
+        <h1 className={styles.title}>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <h2 className={styles.title}>Contacts</h2>
+        <Filter value={filter} onChange={this.changeFilter} />
+        <ContactList
+          contacts={filteredResults}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
+    );
+  }
+}
+
+export default App;
